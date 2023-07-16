9f2187da7196ccb3c8c4cafd38cfce5a3b75c407
Thet Naing Tun <thetnaingtun.ucsy@gmail.com>
2023-07-16 21:02:45 +0700

    configure prisma

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,7 +1,21 @@
+import {PrismaClient} from '@prisma/client'
 import {createServer} from '@graphql-yoga/node' 
+import {NextApiRequest, NextApiResponse} from 'next'
 import {readFileSync} from 'fs'
 import { join } from 'path'
+
 import {Resolvers} from '../../types'
+import prisma from '../../lib/prisma'
+
+export type GraphQLContext = {
+    prisma: PrismaClient
+}
+
+export async function createContext():Promise<GraphQLContext>{
+    return {
+        prisma
+    }
+}
 
 const typeDefs = readFileSync(join(process.cwd(), "schema.graphql"),{
     encoding:"utf-8"
@@ -18,12 +32,16 @@ const resolvers:Resolvers = {
     }
 }
 
-const server = createServer({
+const server = createServer<{
+    req: NextApiRequest,
+    res: NextApiResponse
+}>({
     endpoint: "/api",
     schema:{
         resolvers,
         typeDefs
-    }
+    },
+    context: createContext()
 })
 
 export default server.requestListener;
