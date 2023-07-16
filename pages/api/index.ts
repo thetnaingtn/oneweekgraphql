64cb5b039074a547b089cef4352f04b6cf0a8125
Thet Naing Tun <thetnaingtun.ucsy@gmail.com>
2023-07-16 20:06:56 +0700

    add graphql-codegen

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,12 +1,13 @@
 import {createServer} from '@graphql-yoga/node' 
 import {readFileSync} from 'fs'
 import { join } from 'path'
+import {Resolvers} from '../../types'
 
 const typeDefs = readFileSync(join(process.cwd(), "schema.graphql"),{
     encoding:"utf-8"
 })
 
-const resolvers = {
+const resolvers:Resolvers = {
     Query:{
         cart(_, {id}){
             return{
