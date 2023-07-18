21a8b413e9d32ec1e1fe366e22b5e3534f4a8aff
Thet Naing Tun <thetnaingtun.ucsy@gmail.com>
2023-07-18 12:01:47 +0700

    format

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,17 @@
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
 declare global {
-    var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined;
+}
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
   }
-  
-  let prisma: PrismaClient;
-  
-  if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient();
-  } else {
-    if (!global.prisma) {
-      global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
-  }
-  export default prisma;
+  prisma = global.prisma;
+}
+export default prisma;
